95a2aa3e5176cba626f234d04303ff0a2aa8ef61
Duc Nguyen <triduc2311m@gmail.com>
2020-07-02 22:40:40 -0400

    Add GCloud Credentials for Docker Image. Update GCloud service with pip

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,12 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+
+ENV PORT=5000
+ENV GOOGLE_APPLICATION_CREDENTIALS="/usr/src/app/GCloud_credentials.json"
+
 CMD ["python", "./app.py"]
 
-EXPOSE 5000
+EXPOSE $PORT
 
 
--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import os
+import io
+import sys
+from google.cloud import vision
+from google.cloud.vision import types
 from flask import Flask, flash, redirect, render_template
 
 app = Flask(__name__)
@@ -7,9 +11,34 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route("/home")
+def home():
+    client = vision.ImageAnnotatorClient()
+
+    file_name = os.path.abspath('temp/Miniature-Schnauzer.jpg')
+    file_name = os.path.abspath('temp/corgi.jpg')
+
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Perform label detection
+    response = client.label_detection(image=image)
+    labels=response.label_annotations
+
+    print(labels, file=sys.stderr)
+    
+    #for label in labels:
+     #   print(label.description, file=sys.stderr)
+
+    return render_template("index.html")
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port = port, debug=True)
+    app.run(debug=True, host='0.0.0.0', port = port)
+
 
 
 
